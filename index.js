02bd46e8af7ebd845a95544c39dc51272c6f81b2
Karl Redgate <Karl.Redgate@gmail.com>
2018-01-02 15:39:09 -0500

    add more libraries

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 module.exports = Noi;
+module.exports.Contract = require('./lib/Contract');
+module.exports.Alerts = require('./lib/Alerts');
+module.exports.Events = require('./lib/Events');
 
 const EdgeGrid = require('EdgeGrid');
 
