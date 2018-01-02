1bf552c54a8ef0d8978c70f53adadb490764f94e
Karl Redgate <Karl.Redgate@gmail.com>
2018-01-02 18:22:49 -0500

    add network list api skel

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = Noi;
 module.exports.Contract = require('./lib/Contract');
 module.exports.Alerts = require('./lib/Alerts');
 module.exports.Events = require('./lib/Events');
+module.exports.NetworkList = require('./lib/NetworkList');
 
 const EdgeGrid = require('EdgeGrid');
 
