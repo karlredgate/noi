950d4873b7ff182878db7375808fd25ad8c1b827
Karl Redgate <Karl.Redgate@gmail.com>
2018-02-05 16:39:11 -0500

    remove old temp api definitions lib

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports.Alerts = require('./lib/Alerts');
 module.exports.Events = require('./lib/Events');
 module.exports.TrafficMgmt = require('./lib/TrafficMgmt');
 
-module.exports.ApiDefinitions = require('./lib/ApiDefinitions');
 module.exports.NetworkList = require('./lib/NetworkList');
 module.exports.SIEM = require('./lib/SIEM');
 module.exports.SecMon = require('./lib/SecMon');
